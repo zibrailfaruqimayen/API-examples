bccaf891ee2c67efd79f6f07bf5af5a42422d3ea
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-02-24 01:00:02 +0600

    first commit

---
+++ js/randomuser.js
@@ -0,0 +1,21 @@
+const loadUser = () => {
+  fetch("https://randomuser.me/api/?gender=female")
+    .then((res) => res.json())
+    .then((data) => displayUser(data));
+};
+const displayUser = (user) => {
+  const genderTag = document.getElementById("gender");
+  genderTag.innerHTML = user.results[0].gender;
+
+  const name =
+    user.results[0].name.title +
+    " " +
+    user.results[0].name.first +
+    " " +
+    user.results[0].name.last;
+  document.getElementById("name").innerHTML = name;
+  //   const userImg = document.getElementById("img");
+  //   userImg.innerHTML = user.results.picture;
+  console.log(user);
+};
+loadUser();
